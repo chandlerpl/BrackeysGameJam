d1258b5e2aa5f29b2c4baa1a70329913f7186d2a
ChandlerPopeLewis-PS <c.popelewis@gmail.com>
2021-08-27 15:38:27 +0100

    Updates to lighting

---
+++ Assets/Scripts/Enemy/EndAction.cs
@@ -0,0 +1,24 @@
+using CP.AILibrary.FiniteStateMachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EndAction", menuName = "CP/Action/End")]
+public class EndAction : Action
+{
+    public override void Act(StateMachine stateMachine)
+    {
+        // GameOver stuff?
+
+    }
+
+    public override void Enter(StateMachine stateMachine)
+    {
+
+    }
+
+    public override void Exit(StateMachine stateMachine)
+    {
+
+    }
+}
