688d01e5cde4bd23d1c761b3740f1fbbb1784629
chandlerpl <c.popelewis@gmail.com>
2024-04-12 16:31:21 +0100

    Added Ambient Occlusion

--- Assets/Scripts/Player/MouseMovement.cs
+++ Assets/Scripts/Player/MouseMovement.cs
@@ -1,16 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MouseMovement : MonoBehaviour
 {
-    void Start()
+    public Transform cameraPivot;
+    [SerializeField]
+    private float _lateralSensitivity = 2.0f;
+    [SerializeField]
+    private float _verticalSensitivity = 2.0f;
+    [SerializeField]
+    private float _minPitchAngle = -90.0f;
+
+    [SerializeField]
+    private float _maxPitchAngle = 90.0f;
+
+    protected Quaternion characterTargetRotation;
+    protected Quaternion cameraTargetRotation;
+
+    public Quaternion CharacterRotation { get => characterTargetRotation; }
+
+    private PlayerInput playerInput;
+    private Vector2 _cameraMovement;
+    private void Start()
     {
-        
+        playerInput = GetComponent<PlayerInput>();
+
+        characterTargetRotation = transform.localRotation;
+        cameraTargetRotation = cameraPivot.localRotation;
+
+        playerInput.currentActionMap.FindAction("CameraMove").performed += CameraMove_performed;
+        playerInput.currentActionMap.FindAction("CameraMove").canceled += CameraMove_canceled;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
+
+    private void CameraMove_canceled(InputAction.CallbackContext obj)
+    {
+        _cameraMovement = Vector2.zero;
+    }
+
+    private void CameraMove_performed(InputAction.CallbackContext obj)
+    {
+        _cameraMovement = obj.ReadValue<Vector2>();
+    }
+
 
     void Update()
     {
-        
+        var yaw = _cameraMovement.x * _lateralSensitivity;
+        var pitch = _cameraMovement.y * _verticalSensitivity;
+
+        var yawRotation = Quaternion.Euler(0.0f, yaw, 0.0f);
+        var pitchRotation = Quaternion.Euler(-pitch, 0.0f, 0.0f);
+
+        characterTargetRotation *= yawRotation;
+
+        cameraTargetRotation *= pitchRotation;
+        cameraTargetRotation = ClampPitch(cameraTargetRotation);
+
+        cameraPivot.localRotation = cameraTargetRotation;
+    }
+
+    protected Quaternion ClampPitch(Quaternion q)
+    {
+        q.x /= q.w;
+        q.y /= q.w;
+        q.z /= q.w;
+        q.w = 1.0f;
+
+        var pitch = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x);
+
+        pitch = Mathf.Clamp(pitch, _minPitchAngle, _maxPitchAngle);
+
+        q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * pitch);
+
+        return q;
     }
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,12 +7,23 @@ using UnityEngine.InputSystem;
 //[RequireComponent(typeof())]
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Player Height")]
+    public float normalHeight = 1f;
+    public float crouchHeight = 0.5f;
+
+    [Header("Player Speed")]
+    public float crouchSpeed = 1.5f;
+    public float walkSpeed = 2.5f;
+    public float runSpeed = 4.5f;
 
     private PlayerInput playerInput;
     private Rigidbody rb;
     private MouseMovement mouseMovement;
 
     private Vector2 _playerMovement;
+    private float _speed;
+    private bool _isSprinting;
+    private bool _isCrouching;
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -21,6 +32,41 @@ public class PlayerMovement : MonoBehaviour
 
         playerInput.currentActionMap.FindAction("Move").performed += Move_performed;
         playerInput.currentActionMap.FindAction("Move").canceled += Move_canceled;
+        playerInput.currentActionMap.FindAction("Sprint").performed += Sprint_performed;
+        playerInput.currentActionMap.FindAction("Sprint").canceled += Sprint_performed;
+        playerInput.currentActionMap.FindAction("Crouch").performed += Crouch_performed;
+        playerInput.currentActionMap.FindAction("Crouch").canceled += Crouch_performed;
+
+        _speed = walkSpeed;
+    }
+    private void Sprint_performed(InputAction.CallbackContext obj)
+    {
+        if (obj.phase == InputActionPhase.Performed)
+        {
+            _isSprinting = true;
+            _speed = runSpeed;
+        }
+        else
+        {
+            _isSprinting = false;
+            _speed = walkSpeed;
+        }
+    }
+
+    private void Crouch_performed(InputAction.CallbackContext obj)
+    {
+        if (obj.phase == InputActionPhase.Performed)
+        {
+            _isCrouching = true;
+            _speed = crouchSpeed;
+            transform.localScale = new Vector3(1, crouchHeight, 1);
+        }
+        else
+        {
+            _isCrouching = false;
+            _speed = walkSpeed;
+            transform.localScale = new Vector3(1, normalHeight, 1);
+        }
     }
 
     private void Move_canceled(InputAction.CallbackContext obj)
@@ -37,6 +83,7 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector3 dir = (transform.forward * _playerMovement.y) + (transform.right * _playerMovement.x);
 
-        rb.velocity = dir;
+        rb.velocity = dir * _speed;
+        rb.rotation = mouseMovement.CharacterRotation;
     }
 }
